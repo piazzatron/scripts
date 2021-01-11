08832e3f1145bcc5f47e5f2e82ba55cf3a00e88a
piazzatron <michael.piazza.mp@gmail.com>
2021-01-10 16:48:12 -0800

    Surface random article from Things + email

---
+++ things_script.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Get a random article
+ARTICLE=$(things.sh all | grep "Articles and Videos" | sort -R | head -n 1)
+
+# Get just the title
+TITLE=$(echo $ARTICLE | cut -d '|' -f 2)
+
+# Search for the article to get the whole thing
+SEARCHED=$(things.sh -s "${TITLE}" search)
+
+# Grab the link by sending $SEARCHED into awk,
+# splitting on the delimiter '|', and then printing the last value
+LINK=$(echo $SEARCHED | awk '{n=split($0, a, "|"); print a[n];}')
+
+# Make the email body
+# Configured Postfix using:
+# https://blog.travismclarke.com/post/send-email-from-terminal/
+# Send it
+SUBJECT="Things Article of the day: $TITLE"
+BODY="
+  $TITLE </br>
+  <a href="$LINK">$LINK</a> </br>
+  </br>
+  w love,</br>
+  - you
+"
+(
+  echo To: michael.piazza@hey.com
+  echo "Content-Type: text/html; "
+  echo "Subject: $SUBJECT"
+  echo
+  echo $BODY
+) | sendmail -t
+# echo "Your article of the day!
+
+#   $TITLE
+#   <a>$LINK</a>
+
+#   w love,
+#   - you
+# " | mail -a="Content-type: text/html" -s "$SUBJECT" "michael.piazza@hey.com"
+
+
